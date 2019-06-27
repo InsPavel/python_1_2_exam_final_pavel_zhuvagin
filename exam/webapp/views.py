3bdaf46772c45ea79fc93f303c214ecb9ea42aef
pavel <zhpv07@gmail.com>
2019-06-26 20:36:43 -0400

    Подключён staic, добавлен superuser и базовый шаблон.

--- exam/webapp/views.py
+++ exam/webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class ArticleListView(TemplateView):
+    template_name = 'article_list.html'
